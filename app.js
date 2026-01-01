6b1535e792a5dbc7de9ebeeea638726f9aae10f9
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2026-01-01 23:41:40 +0500

    typeof test

---
+++ app.js
@@ -0,0 +1,19 @@
+//Javascrit data types
+//String    ""
+//Boolean True/False
+//NUmber   38929
+//Arrary  [a,d,f]
+//Object  object={key : value} value kuck bhi ho string boolean number array function ya ke or boject
+
+//Javascript Six False values
+//1. false
+//2. 0
+//3. ""
+//4. null
+//5. undefined
+//6. NaN
+//in sab ke ilawa baki sab Values true hen
+
+let a ="23as"
+let cunvertedValueInNumber=Number(a)
+console.log(typeof a);
