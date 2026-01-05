0f6a49fbfb7066e09a293d5155fc5b393e154a07
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2026-01-05 21:13:04 +0500

    Update

--- app.js
+++ app.js
@@ -13,13 +13,25 @@
 //5. undefined
 //6. NaN
 //in sab ke ilawa baki sab Values true hen
-let myCheck = document,getElementById("CheckBox")
-let a ="23as"
+// let myCheck = document,getElementById("CheckBox")
+// let a ="23as"
 // let cunvertedValueInNumber=Number(a)
 // console.log(cunvertedValueInNumber);
 // alert("Hello World")
  
-const newElement = document.createElement("div");
-newElement.textContent = "Hello";
-document.body.appendChild(newElement);
-alert( +"value is cheaked")
+// const newElement = document.createElement("div");
+// newElement.textContent = "Hello";
+// document.body.appendChild(newElement);
+// alert( +"value is cheaked")
+// let a= 10
+// console.log(a--);postincrement
+// console.log(a);
+// let b=20;
+// console.log(--b);preincrement
+let a=10
+let b=20
+let c=50
+// console.log(++b);
+
+// console.log((a--)-(++b)+(c++));
+// 10-20+50= -11+50=39
